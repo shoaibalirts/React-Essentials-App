2a1b00735cf885f76ee90a80ed2247e9821a5bc5
shoaibalirts <shoaibali.rts@gmail.com>
2024-07-14 11:30:01 +0200

    Updated App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,55 +1,28 @@
-import MainGoal from "./MainGoal";
+const reactDescriptions = ['Fundamental', 'Crucial', 'Core'];
+
+function genRandomInt(max) {
+  return Math.floor(Math.random() * (max + 1));
+}
+
+function Header() {
+  return (
+    <header>
+      <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
+      <h1>React Essentials</h1>
+      <p>
+        Fundamental React concepts you will need for almost any app you are going to build!
+      </p>
+    </header>
+  );
+}
 
 function App() {
   return (
-    <div id="app">
-      <h1>Time to Practice!</h1>
-      <p>
-        Build a <code>&lt;MainGoal&gt;</code>
-        <MainGoal
-          goal={"Build a MainGoal component and insert it below this text."}
-        />
-      </p>
-      <p>
-        Your <code>&lt;MainGoal&gt;</code>
-        <MainGoal
-          goal={
-            "component should simply output some text that describes your main course goal (e.g., "
-          }
-        />
-        &quot;
-        <MainGoal goal={"My main goal: Learn React in great detail"} />
-        &quot;).
-      </p>
-      <p>
-        <strong>
-          <MainGoal goal={"Important"} />:
-        </strong>{" "}
-        <MainGoal goal={"You custom component must contain the text"} />
-        &quot;
-        <MainGoal goal={"My main goal"} />
-        :&quot;
-      </p>
-      <p>
-        <strong>
-          <MainGoal goal={"Also important:"} />
-        </strong>{" "}
-        <MainGoal goal={"For the automatic checks to succeed, you"} />{" "}
-        <strong>must export</strong>{" "}
-        <MainGoal
-          goal={
-            "your custom component function! Not as a default but simply by adding the "
-          }
-        />
-        <code>export</code>{" "}
-        <MainGoal goal={"keyword in front of your function (e.g., "} />{" "}
-        <code>
-          <MainGoal goal={"export function YOUR_COMPONENT_NAME"} />
-        </code>
-        ).
-      </p>
-      {/* DON'T CHANGE THE TEXT / CONTENT ABOVE */}
-      {/* OUTPUT YOUR COMPONENT HERE */}
+    <div>
+      <Header />
+      <main>
+        <h2>Time to get started!</h2>
+      </main>
     </div>
   );
 }
