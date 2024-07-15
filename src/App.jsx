70b583c457cb83be522e1a657e763ac390c712ef
shoaibalirts <shoaibali.rts@gmail.com>
2024-07-15 10:39:56 +0200

    Add a Card Component

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { CORE_CONCEPTS } from "../src/utility/data.js";
 import Header from "../src/components/Header/Header.jsx";
 import CoreConcept from "./components/CoreConcept/CoreConcept.jsx";
 import TabButton from "./components/TabButton/TabButton.jsx";
+import Card from "./components/Card/Card.jsx";
 console.log(CORE_CONCEPTS);
 function App() {
   return (
@@ -30,6 +31,28 @@ function App() {
             <TabButton>Props</TabButton>
             <TabButton>State</TabButton>
           </menu>
+        </section>
+        <section>
+          <h1>Available Experts</h1>
+          <Card name="Anthony Blake">
+            <p>
+              Blake is a professor of Computer Science at the University of
+              Illinois.
+            </p>
+            <p>
+              <a href="mailto:blake@example.com">Email Anthony</a>
+            </p>
+          </Card>
+
+          <Card name="Maria Miles">
+            <p>
+              Maria is a professor of Computer Science at the University of
+              Illinois.
+            </p>
+            <p>
+              <a href="mailto:blake@example.com">Email Maria</a>
+            </p>
+          </Card>
         </section>
       </main>
     </div>
