d1470c96bb87ecf987b0dbc07b0c6d5b2939c261
shoaibalirts <shoaibali.rts@gmail.com>
2024-07-17 09:51:18 +0200

    all files have to be deleted

--- src/components/Card/Card.jsx
+++
@@ -1,9 +0,0 @@
-import "./Card.css";
-export default function Card({ children, name }) {
-  return (
-    <div className="card">
-      <h2>{name}</h2>
-      {children}
-    </div>
-  );
-}
--- src/components/TabContent.jsx
+++
@@ -1,3 +0,0 @@
-export default function TabContent() {
-  return "hello";
-}
